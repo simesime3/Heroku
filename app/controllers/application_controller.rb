41844d6d8c728c510c79e64478dfe7bb5f7c78aa
古賀克磨 <kogakatsuma@kogakatsumanoMacBook-Pro.local>
2019-04-20 15:44:18 +0900

    20190420 課題；ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery  with: :exception
+  helper_method :current_user, :logged_in?
 
+  protect_from_forgery  with: :exception
   add_flash_types :sucess, :info, :warning, :danger
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
