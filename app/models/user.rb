a099e7d02feb66a1e0fba3dc7663ea9c34c53c03
古賀克磨 <kogakatsuma@kogakatsumanoMacBook-Pro.local>
2019-04-21 20:41:07 +0900

    質問投稿

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates  :password, presence: true, length: { minimum: 8, maximum: 32},  format: { with: VALID_PASSWORD_REGEX}
 
   has_secure_password
+
+  has_many  :topics
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'topics/new'
   get 'sessions/new'
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   get 'pages/help'
 
   resources :users
+  resources :topics
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
